f23c202af8fd356a72051217bea8e2b178b6a4e0
Banu <banu2106@gmail.com>
2023-02-28 00:37:44 +0300

    a

---
+++ src/day03_EscapeSequences/Face.java
@@ -0,0 +1,13 @@
+package day03_EscapeSequences;
+
+public class Face {
+    public static void main(String[] args) {
+        System.out.println("\t\t\t +\" \" \" \" \"+");
+        System.out.println("\t\t\t[|  o   o  |]");
+        System.out.println("\t\t\t |    ^    |");
+        System.out.println("\t\t\t |   '_'   |");
+        System.out.println("\t\t\t +---------+");
+
+
+    }
+}
