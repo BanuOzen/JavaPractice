19da6a5a95f416b43f4cc877a40900d8dad5fe19
Banu <banu2106@gmail.com>
2023-03-16 14:37:15 +0300

    day-12

--- src/day12_Scanner/StockMarket.java
+++ src/day12_Scanner/StockMarket.java
@@ -1,7 +1,36 @@
 package day12_Scanner;
 
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class StockMarket {
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many total shares they have already? ");
+        int stock= input.nextInt();
+
+        if(stock>0){
+            System.out.println("How much their total value in the stock market? ");
+            double totalValue=input.nextDouble();
+
+            input.nextLine();
+            System.out.println("Enter the name of the company:");
+            String company=input.nextLine();
+
+            System.out.println("Your total stock market holding is $" + totalValue + " Which is made up of " + stock + " shares." +
+                    company + " is your company holdings.");
+
+
+
+        }else{
+            System.out.println("Invalid value");
+        }
+
+
+
+
 
     }
 }
