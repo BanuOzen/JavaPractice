17f12376f2e3a9ee341dcee05aff29f5cf9c6489
Banu <banu2106@gmail.com>
2023-03-17 15:24:08 +0300

    day-13-String

---
+++ src/day13_String/Initials.java
@@ -0,0 +1,38 @@
+package day13_String;
+
+import java.util.Scanner;
+
+public class Initials {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your first name:");
+        String first= input.next();
+
+        System.out.println("Enter your last name:");
+        String last = input.next();
+
+        char f = first.charAt(0);
+        char l = last.charAt(0);
+
+        String initials= "" + f +"."+l;
+
+        System.out.println("initials = " + initials);
+        input.close();
+
+
+    }
+}
+/*
+4. write a program that can return the initials of the user
+
+            ex:
+                cybertek
+                school
+
+            output:
+                C.S
+
+        Note: Pay attention to the example output
+
+ */
