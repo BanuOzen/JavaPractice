986471a77e6225f867027ae75f3a73e21096d2d5
Banu <banu2106@gmail.com>
2023-03-19 00:29:22 +0300

    day-13-String

---
+++ src/day13_String/TipCalculator.java
@@ -0,0 +1,63 @@
+package day13_String;
+
+import java.util.Scanner;
+
+public class TipCalculator {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter to split or No split(Yes or No):");
+        String split=scan.next();
+
+        System.out.println("Enter to number of people : ");
+        int people=scan.nextInt();
+
+        System.out.println("Enter to check amount:");
+        double check=scan.nextDouble();
+
+        System.out.println("Enter to service quality:");
+        String quality= scan.next();
+
+
+    }
+}
+/*2. Create a class called TipCalculator, write a program for a tip calculator.
+There will be different service quality benchmarks that will determine the tip given.
+There will also the ability to calculate based on the number of people in the party and if there is a split of the bill or not.
+
+		Poor = 5%, Fair = 10%, Good = 15%, Great = 20%, Excellent = 25%
+
+	The program should display the following information based on the user input:
+
+	 Split or No split (Yes or No),
+	 Number of people entered: (number) (each person = & in output)
+	 Check amount: (number)
+	 Service Quality: (String)
+	 Total to pay:
+	 Total tip:
+	 Total per person:
+	 Tip per person:
+
+	Ex:
+		Split or No split (Yes or No)?
+		Yes
+		Enter the number of people
+		4
+		Enter the check amount:
+		476
+		How was the srvice quality? (Excellent/Great/Good/Fair/Poor)
+		Excellent
+
+		output:
+			Number of people entered: 4
+			Total to pay: 595.0
+			Total tip: 119.0
+			Total per person: 148.75
+			Tip per person: 29.75
+
+	HINT: you will need to use .equals() method
+
+
+
+ */
