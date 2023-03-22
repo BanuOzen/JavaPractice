e0078549a1acbfb47654fa9867f82dc93368ef82
Banu <banu2106@gmail.com>
2023-03-22 08:20:17 +0300

    day-14-String

---
+++ src/day15_ForLoop/EvenNumbers.java
@@ -0,0 +1,17 @@
+package day15_ForLoop;
+
+public class EvenNumbers {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=100 ; i++) {
+            if(i%2==0){
+                System.out.print(i +" ");
+            }
+
+
+        }
+    }
+}
+/*
+1. Write a program that can return the sum of even numbers between 1 to 100
+ */
---
+++ src/day15_ForLoop/OddNumbers.java
@@ -0,0 +1,16 @@
+package day15_ForLoop;
+
+public class OddNumbers {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <=100 ; i++) {
+            if(i%2==1){
+                System.out.print(i +" ");
+            }
+        }
+
+    }
+}
+/*
+2. Write a program that can return the sum of odd numbers between 1 to 100
+ */
