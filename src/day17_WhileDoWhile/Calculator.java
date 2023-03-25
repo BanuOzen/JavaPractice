0a6f349d46cfed580d45e8531b18eba6fa978c49
Banu <banu2106@gmail.com>
2023-03-25 16:53:49 +0300

    day17-While&DoWhile

--- src/day17_WhileDoWhile/Calculator.java
+++ src/day17_WhileDoWhile/Calculator.java
@@ -1,28 +1,17 @@
 package day17_WhileDoWhile;
-
-import java.util.Scanner;
-
 public class Calculator {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        int a= 50;
+        int b= 9;
 
-        System.out.println("Enter First Number:");
-        int num1= scan.nextInt();
+        int count =0;
 
-        System.out.println("Enter Second Number:");
-        int num2= scan.nextInt();
+        while(a>=b){
+            a-=b;
+            count++;
 
-        System.out.println("Enter a match operator: ");
-        char ch=scan.next().charAt(0);
-
-        while( !(ch=='+' || ch=='-') ){
-            System.err.println("Invalid Operator, Please re-enter");
-            ch=scan.next().charAt(0);
         }
-
-        if(num1>0 && num2>0){
-            System.out.println(num1+num2);
-        }
+        System.out.println(count + " with a remainder of "+a);
 
 
     }
