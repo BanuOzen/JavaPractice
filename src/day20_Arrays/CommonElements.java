0ffeeea6fe973f328d2f0d8eaa9ed3c4a5bc0530
Banu <banu2106@gmail.com>
2023-04-04 16:08:45 +0300

    day20_Arrays

--- src/day20_Arrays/CommonElements.java
+++ src/day20_Arrays/CommonElements.java
@@ -6,9 +6,11 @@ public class CommonElements {
         int[] arr2 = {4, 5, 6, 7, 8};
 
         for (int i = 0; i < arr1.length; i++) {
+            
             for (int j = 0; j < arr2.length; j++) {
+
                 if(arr1[i]==arr2[j]){
-                    System.out.println(arr1[i]);
+                    System.out.print(arr1[i] + " ");
                 }
             }
         }
