9d88dd0604e2c84dea875736d2206dfd5f55004e
Banu <banu2106@gmail.com>
2023-04-06 00:30:28 +0300

    day23_CustomMethodsVoid

--- src/day23_CustomMethodsVoid/CalculateAreaCircle.java
+++ src/day23_CustomMethodsVoid/CalculateAreaCircle.java
@@ -8,7 +8,7 @@ public class CalculateAreaCircle {
     }
     public static void areaOfCircle(double radius){
         double area = radius*radius*3.14;
-        System.out.println(area);
+        System.out.println("area = " + area);
     }
 }
 /*
