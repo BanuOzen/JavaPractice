b77aa48a5f5b92630991d11c3c49b096e1236f0c
Banu <banu2106@gmail.com>
2023-04-06 00:32:58 +0300

    day23_CustomMethodsVoid

---
+++ src/day23_CustomMethodsVoid/CalculateSquare.java
@@ -0,0 +1,18 @@
+package day23_CustomMethodsVoid;
+
+public class CalculateSquare {
+    public static void main(String[] args) {
+
+        squareOfArea(6);
+    }
+    public static void squareOfArea(double side){
+        double area = side * side;
+        System.out.println("area = " + area);
+
+
+    }
+
+}
+/*
+7. create a method that can calculate the area of a square
+ */
