2312333352a8e3fe454b26bdd31472a35522091d
Banu <banu2106@gmail.com>
2023-04-06 00:29:50 +0300

    day23_CustomMethodsVoid

---
+++ src/day23_CustomMethodsVoid/CalculateAreaCircle.java
@@ -0,0 +1,16 @@
+package day23_CustomMethodsVoid;
+
+public class CalculateAreaCircle {
+    public static void main(String[] args) {
+
+        areaOfCircle(4.5);
+
+    }
+    public static void areaOfCircle(double radius){
+        double area = radius*radius*3.14;
+        System.out.println(area);
+    }
+}
+/*
+6. create a method that can calculate the area of a circle
+*/
