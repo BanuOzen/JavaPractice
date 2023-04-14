71cf76293a31fdc6cfa284b098edadfa0cafae2f
Banu <banu2106@gmail.com>
2023-04-14 14:34:39 +0300

    day27_WrapperClasses

--- src/day27_WrapperClasses/InsertElement.java
+++ src/day27_WrapperClasses/InsertElement.java
@@ -18,6 +18,60 @@ public class InsertElement {
 
         return result;
     }
+    public static double[] insertElement(double[] arr, int index, double element){
+        if(index < 0 || index > arr.length){
+            System.err.println("Invalid Index");
+            System.exit(0);
+        }
+        double[] result = new double[arr.length+1];
+        result[index]=element;
+        for (int i = 0, j=0; i < arr.length; i++,j++) {
+            if(i==index){
+                j++;
+            }
+            result[j]=arr[i];
+
+        }
+        return result;
+    }
+
+    public static char[] insertElement(char[] arr, int index, char element){
+        if(index < 0 || index > arr.length){
+            System.err.println("Invalid Index");
+            System.exit(0);
+        }
+
+        char[] result = new char[arr.length+1];
+        result[index]=element;
+
+        for (int i = 0, j=0; i < arr.length; i++, j++) {
+            if(i==index){
+                j++;
+            }
+            result[j]=arr[i];
+        }
+        return result;
+    }
+
+    public static String[] insertElement(String[] arr, int index, String element){
+
+        if(index < 0 || index > arr.length){
+            System.err.println("Invalid Index");
+            System.exit(0);
+        }
+
+        String[] result = new String[arr.length+1];
+        result[index]=element;
+
+        for (int i = 0, j=0; i < arr.length; i++, j++) {
+            if(i==index){
+                j++;
+            }
+            result[j]=arr[i];
+        }
+        return result;
+    }
+
 
 
 }
