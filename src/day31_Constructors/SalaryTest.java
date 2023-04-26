6d53d0ce26afabf3028ee463837af53d8b427c49
Banu <banu2106@gmail.com>
2023-04-26 23:58:14 +0300

    day28_ArrayList

---
+++ src/day31_Constructors/SalaryTest.java
@@ -0,0 +1,15 @@
+package day31_Constructors;
+
+public class SalaryTest {
+    public static void main(String[] args) {
+
+        SalaryCalculator obj = new SalaryCalculator(65,40,5.4, 25.5);
+
+        System.out.println(obj);
+
+        SalaryCalculator obj2 = new SalaryCalculator(5,2,0.1,0.3);
+
+        System.out.println(obj2);
+
+    }
+}
---
+++ src/day31_Constructors/TestAddress.java
@@ -0,0 +1,8 @@
+package day31_Constructors;
+
+public class TestAddress {
+    public static void main(String[] args) {
+        Address address1 = new Address("7925A", "Jones Branch Dr", "McLean", "VA", "22012");
+        System.out.println(address1);
+    }
+}
