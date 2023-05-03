51473ee4283e446abdf69f95356aa5e9f9880533
Banu <banu2106@gmail.com>
2023-05-03 21:07:58 +0300

    day35_Encapsulation

---
+++ src/day35_Encapsulation/PizzaTest.java
@@ -0,0 +1,11 @@
+package day35_Encapsulation;
+
+public class PizzaTest {
+
+    public static void main(String[] args) {
+
+
+        Pizza pizza = new Pizza("small", 3, 4);
+        System.out.println(pizza);
+    }
+}
